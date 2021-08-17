ac95fa86476c0e0abe1096465237603f8c43584b
Healer404 <dukaroniel0701@gmail.com>
2021-08-17 19:44:23 +0800

    CreateTodo.js working now
    It can now get the data from the input of the form whenever press the submit button. The data can only be shown on console.

--- src/CreateTodo.js
+++ src/CreateTodo.js
@@ -4,19 +4,17 @@ import { useForm } from 'react-hook-form';
 export const CreateTodo = () => {
 
   const {register, handleSubmit} = useForm();
-
-  const onSubmit = handleSubmit((data) => {
-    alert(JSON.stringify(data));
-  });
+  
+  const onSubmit = (data) => console.log(data);
 
   return (
         <div className="container">
           <div className="mt-3">
             <h3>Create ToDo Item</h3>
-            <form onSubmit={onSubmit}>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="form-group">
                 <label htmlFor="text">Text: </label>
-                <input ref={register} type="text" name="text" id="text" className="form-control"/>
+                <input {...register("text")} type="text" name="text" id="text" className="form-control"/>
               </div>
               <div className="form-group">
                 <button type="submit" className="btn btn-primary">Create Todo</button>
