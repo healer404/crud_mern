0f061fcb9e6926ee6444ba5b9e918448fe33441f
Healer404 <dukaroniel0701@gmail.com>
2021-08-17 16:14:19 +0800

    Updated the App.js
    - Imported React
    - Imported the Route, Switch, and Link from react-router-dom
    - Finished the layout of the navigation section along its routings

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      // Navigation Section Holder
+      <div>
+        // Navigation Section
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          // Unordered list of the navigation links
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item"><Link to="./" className="nav-item">Todos</li> // Navigation Link Item
+
+            <li className="navbar-item"><Link to="./create" className="nav-item">Create Todo</li> // Navigation Link Item
+          </ul>
+          // END Unordered list of the navigation links
+        </nav>
+        // END Navigation Section
+      </div>
+      // END Navigation Section Holder
+
+    // ***** act as the navigation configuration something ***** //
+    <Switch>
+      <Route exact path="./" component={TodoList}/> // Will act as the home
+      <Route exact path="./edit/:id" component={EditTodo}/> // Will act as the edit todo item
+      <Route exact path="./create" component={CreateTodo}/> // Will act as the create todo item
+    </Switch>
+    // ***** END act as the navigation configuration something ***** //
   );
 }
 
