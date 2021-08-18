4454ef96d652d56e853ada13413732a86fcee073
Healer404 <dukaroniel0701@gmail.com>
2021-08-18 15:25:36 +0800

    Update EditTodo.js
    Provide a form layout for editing the ToDo item

--- src/EditTodo.js
+++ src/EditTodo.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
 
 export const EditTodo = () => {
+  const [todo, setTodo] = useState();
+
+  const {register, handleSubmit} = useForm({
+    defaultValues: { text: todo ? todo.text : "" },
+  });
+
+  const history = useHistory();
+
+  useEffect(() => {
+    setTodo({
+      text: 'food'
+    });
+  }, []);
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+    alert(JSON.stringify(data));
+    history.push('/');
+  });
   return (
-      <div className="container">Edit ToDo Item</div>
-  )
-}
+      <div className="container">
+        <div className="mt-3">
+          <h3>Edit ToDo Item</h3>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group">
+              <label htmlFor="text">Text: </label>
+              <input {...register("text")} type="text" name="text" id="text" className="form-control"/>
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary">Create Todo</button>
+            </div>
+          </form>
+        </div>
+      </div>
+  );
+};
