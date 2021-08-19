72ec994ef314a5e30286168306042fa3eb220e2a
Healer404 <dukaroniel0701@gmail.com>
2021-08-19 12:59:47 +0800

    Update on Server.js
    Added codes for establishing connection to the MongoDB database

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://127.0.0.1:27017/todos', {useNewUrlParser: true});
+
+mongoose.connection.once('open', () => {
+    console.log("MongoDB connection established successfully.");
+});
+
+const PORT = 4000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log("Server is running on PORT" + PORT);
+});
+
+
